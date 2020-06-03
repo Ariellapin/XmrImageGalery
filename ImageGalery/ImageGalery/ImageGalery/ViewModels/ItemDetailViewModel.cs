ceca35a06e28d3c0265807fb386a3e3341239b08
Ariel Lapinin <ariel.l@sbtech.com>
2020-06-03 09:24:23 +0300

    add

---
+++ ImageGalery/ImageGalery/ImageGalery/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ImageGalery.Models;
+
+namespace ImageGalery.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ ImageGalery/ImageGalery/ImageGalery/Views/ChooseFile.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Plugin.FilePicker;
+using Plugin.FilePicker.Abstractions;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ImageGalery.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ChooseFile : ContentPage
+    {
+        public ChooseFile()
+        {
+            InitializeComponent();
+            //OpenFile().Wait();
+        }
+
+        
+
+        private async void OpenFile(object sender, EventArgs e)
+        {
+            try
+            {
+                FileData fileData = await CrossFilePicker.Current.PickFile();
+                if (fileData == null)
+                    return; // user canceled file picking
+
+                FileInfo fi = new FileInfo(fileData.FilePath);
+                var files = fi.Directory.GetFiles("*.jpg", SearchOption.AllDirectories).Select(f=>new {date=f.LastWriteTime,name=f.Name });
+                Console.WriteLine(files.Count());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception choosing file: " + ex.ToString());
+                await DisplayAlert("Alert", "Exception choosing file: " + ex.ToString(), "OK");
+            }
+        }
+    }
+}
